c6f2faadfb325b96e91651d9411ae8ad6b7759bd
Doug Bell <doug.bell@grantstreet.com>
2019-11-18 22:34:01 -0500

    initial implementation

---
+++ lib/App/lrrr.pm
@@ -0,0 +1,28 @@
+package App::lrrr;
+# ABSTRACT: Little Restart Runner (Really)
+# VERSION
+
+=head1 SYNOPSIS
+
+    lrrr [--watch|-w <dir>]... <command>
+    lrrr --help|-h
+
+=head1 DESCRIPTION
+
+This program will watch one or more directories and re-run the given
+command when the contents of the files in those directories changes.
+
+See L<lrrr> for detailed documentation.
+
+=head1 SEE ALSO
+
+L<Mojo::Server::Morbo>
+
+=head1 AUTHOR
+
+(E<0xa9> 2019) Sebastian Riedel and the Mojolicious developers
+(E<0xa9>) Grant Street Group
+
+=cut
+
+1;
